c8109b4b3eae541d5b1d829d0f10801e4a6faacd
Min Zayar Maung <minzayarmaung2002@gmail.com>
2024-08-10 19:57:44 +0630

    test

---
+++ unicodeMyanmar/src/main/java/com/unicodeMM/unicodeMyanmar/UnicodeMyanmarApplication.java
@@ -0,0 +1,13 @@
+package com.unicodeMM.unicodeMyanmar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnicodeMyanmarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UnicodeMyanmarApplication.class, args);
+	}
+
+}
